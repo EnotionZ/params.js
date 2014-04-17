5166829f721acda7df2fe2adb704bad1b54515c8
Dominick Pham <dominick.pham@gmail.com>
2014-04-17 14:06:36 -0700

    settings panel + toggle

--- params.js
+++ params.js
@@ -1,9 +1,22 @@
 (function(root) {
 
-	var PARAMS_KEY = '_params_';
 	var _events = {};
+
+	var PARAMS_KEY = '_params_';
 	var _paramsStr = localStorage.getItem(PARAMS_KEY);
 	var _params =  _paramsStr && JSON.parse(_paramsStr) || {};
+
+	var $css = $('<style/>').appendTo('body').html('.'+PARAMS_KEY+'{'+
+		'display: none;'+
+		'position: absolute; z-index: 99999;'+
+		'left: 20px; right: 20px; top: 20px; bottom: 20px;'+
+		'background: #fff;'+
+		'border-radius: 4px;'+
+		'box-shadow: 0 0 5px rgba(0,0,0,.8);'+
+		'}\n .active.'+PARAMS_KEY+'{ display: block; }');
+
+	var $settings = $('<div/>').addClass(PARAMS_KEY).appendTo('body');
+
 	var pub = {};
 
 	root.params = function(key, val) {
@@ -14,14 +27,17 @@
 
 	pub.reset = function() { localStorage.removeItem(PARAMS_KEY); };
 
-	pub.get = function(key) { return _params[key]; };
+	pub.get = function(key) {
+		if(typeof key === 'undefined') return _params;
+		return _params[key];
+	};
 
 	pub.set = function(key, val) {
 		var current = _params[key];
 
 		if(current !== val) {
 			_params[key] = val;
-			localStorage.setItem(PARAMS_KEY, JSON.stringify(_params));
+			localStorage.setItem(PARAMS_KEY, JSON.stringify(_params, null, 4));
 
 			var evStack = _events[key];
 			if(Array.isArray(evStack)) {
@@ -32,11 +48,14 @@
 		return pub;
 	};
 
-	pub.add = function(info) {
-		if(Array.isArray(info)) {
-			info.forEach(add);
-		} else {
+	var _add = function(info) {
+		if(typeof pub.get(info.key) === 'undefined') {
+			pub.set(info.key, info.value);
 		}
+	};
+	pub.add = function(info) {
+		if(Array.isArray(info)) info.forEach(_add);
+		else _add(info);
 		return pub;
 	};
 
@@ -46,10 +65,12 @@
 		if(evStack.indexOf(fn) === -1) evStack.push(fn);
 	};
 
-	pub.render = function() {
-	};
+	pub.hide = function() { $settings.removeClass('active'); };
+	pub.show = function() { $settings.addClass('active'); };
+	pub.toggle = function() { $settings.toggleClass('active'); };
 
 	$.extend(root.params, pub);
 
+	$('body').on('keypress', function(e) { if(e.keyCode === 63) pub.toggle(); });
 
 })(window);
